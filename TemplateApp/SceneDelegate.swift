7e72682622a2d5bfd3a50f3b98399efe6ef7203a
arai <niimaru09@gmail.com>
2022-06-20 20:30:12 +0900

    add

--- TemplateApp/SceneDelegate.swift
+++ TemplateApp/SceneDelegate.swift
@@ -13,9 +13,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+
+        // この３行で、キーボード以外の場所をタップするとキーボードを閉じたり、textfieldが自動的にキーボードより上のように位置するようにしている。
+//        IQKeyboardManager.shared.enable = true
+//        IQKeyboardManager.shared.enableAutoToolbar = false
+//        IQKeyboardManager.shared.shouldResignOnTouchOutside = true
+        
         guard let _ = (scene as? UIWindowScene) else { return }
     }
 
